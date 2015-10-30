87cc70a7181aa1233f9949b806286b6a308efe21
ed42311 <ed42311@gmail.com>
2015-10-30 15:32:52 -0600

    transfering react button over to the main file structure

--- public/app.js
+++ public/app.js
@@ -1,25 +1,102 @@
+var App = React.createClass({
+
+    getInitialState: function() {
+        return {ingredients: false, recipes: false};
+    },
+
+    handleIngredients: function(event) {
+          console.log('---')
+        this.setState({ingredients: !this.state.ingredients});
+    },
+    handleRecipes: function(event) {
+        this.setState({recipes: !this.state.recipes});
+    },
+    render: function() {
+        var recipesText = this.state.recipes ? <AllRecipes handleIngredients={this.handleIngredients}/> : "";
+        var ingredientsText = this.state.ingredients ? <Ingredients /> : "";
+        return (
+        <div>
+        <h1>Crisper searching stuff</h1>
+           <button onClick={this.handleRecipes} type="button" className="btn btn-default">Search for Chicken</button> 
+              {recipesText}                 
+              {ingredientsText}
+            </div>
+        );
+    }
+  }),  
+
+var RecipeBox = React.createClass({
+
+  getInitialState: function (){
+    return {data: []}
+  },
+
+    loadRecipesFromServer: function(recipe){
+      console.log(this.state),
+      $.ajax({
+        url: this.props.url + recipe.foodItem,
+        dataType: 'json',
+        cache: false,
+        success:function(data){
+          console.log("recipe success")
+          this.setState({data:data});
+        }.bind(this),
+        error: function(xhr, status, err){
+          console.log("broken" + this.props.url)
+          console.error(this.props.url, status, err.toString());
+        }.bind(this)
+      });
+
+    },
+
+    loadIngredientsFromServer: function(rId){
+      $.ajax({
+        url: this.props.url2 + rId,
+        dataType: 'json',
+        cache: false,
+        success:function(data){
+
+          console.log("ingredient success")
+          this.setState({data:data});
+        }.bind(this),
+        error: function(xhr, status, err){
+          console.log("broken " + this.props.url)
+          console.error(this.props.url, status, err.toString());
+        }.bind(this)
+      });
+
+    },
+    
+        render: function() {
+            return (
+              <div>
+                <RecipeForm onRecipeSubmit={this.loadRecipesFromServer} />
+                <RecipeList data={this.state.data}/>
+                <IngredientList onRecipeSubmit={this.loadIngredientsFromServer} />
+              </div>
+            );
+        }
+    }),
+
 var RecipeList = React.createClass({
   render: function() {
      var recipeData = this.props.data.map(function(r){
-      console.log(r)
       return ( 
         <div>
-
-            <div className="col-md-6 col-sx-10 col-sm-8 col-lg-4 col-xs-offset-1" id="panel-spacing">
-              <div className="panel panel-default" id="panel">
-                <div className="panel-heading" id="panel-heading">
+          <div className="col-md-6 col-sx-10 col-sm-8 col-lg-4 col-xs-offset-1" id="panel-spacing">
+            <div className="panel panel-default" id="panel">
+              <div className="panel-heading" id="panel-heading">
                 <h6 className="panel-title"><a href={r.source_url}> {r.title} </a></h6>
+              </div>
+              <div className="panel-body">
+                <li> <img src={r.image_url} id="thumbnail"/> </li>
+              </div>
             </div>
-            <div className="panel-body">
-          <li> <img src={r.image_url} id="thumbnail"/> </li>
-                  </div>
-                </div>
-            </div>
+          </div>
         </div>
-      );
+      );   
+})
 
-      
-    })
     return (
       <div>
         <div className="col-md-12 text-center">
@@ -56,40 +133,9 @@ var RecipeForm = React.createClass({
         }
     });
 
-var RecipeBox = React.createClass({
 
-  getInitialState: function (){
-    return {data: []}
-  },
 
-    loadRecipesFromServer: function(recipe){
-      console.log(this.state),
-      $.ajax({
-        url: this.props.url + recipe.foodItem,
-        dataType: 'json',
-        cache: false,
-        success:function(data){
-          console.log("recipe success")
-          this.setState({data:data});
-        }.bind(this),
-        error: function(xhr, status, err){
-          console.log("broken" + this.props.url)
-          console.error(this.props.url, status, err.toString());
-        }.bind(this)
-      });
-
-    },
-        render: function() {
-            return (
-              <div>
-                <RecipeForm onRecipeSubmit={this.loadRecipesFromServer}/>
-                <RecipeList data={this.state.data}/>
-              </div>
-            );
-        }
-    });
-
-    React.render(<RecipeBox url="/api/recipes/"/>, document.getElementById('searchBar'));
+React.render(<RecipeBox url="/api/recipes/" url2="/api/recipe/"/>, document.getElementById('searchBar'));
 
 
 
